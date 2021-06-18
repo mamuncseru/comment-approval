aac2bf5dfe58d739723e8d6206466fa504d13b85
mamuncseru <mamun.cseian@gmail.com>
2021-06-18 16:49:25 +0600

    Create a react app which provide a beautiful comment approval interface

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import CommentDetail from './commentDetail';
+import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    return (
+        <div className='ui container comments'>
+            <ApprovalCard>
+                <CommentDetail 
+                    author='Alex'
+                    timeAgo="Today at 2:00AM" 
+                    commentText='Great Post!'
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author='Sam' 
+                    timeAgo="Today at 4:45PM" 
+                    commentText='Nice blog post!' 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author='Jane'
+                    timeAgo="Yesterday at 3:00PM" 
+                    commentText='Excellent Post!'
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
